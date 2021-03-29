eb2b30771837fd5da6a75d1818542ac1d4873e3d
alexret <alex.relano18@gmail.com>
2021-03-29 12:00:00 +0200

    Initial commit

---
+++ src/main/java/com/example/fatdown/FatDownApplication.java
@@ -0,0 +1,13 @@
+package com.example.fatdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatDownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatDownApplication.class, args);
+	}
+
+}
