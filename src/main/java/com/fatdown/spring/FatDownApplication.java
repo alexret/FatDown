745514ef90b5f3ba9e951a641d50bdcd80c45193
amseguraglez <58846324+amseguraglez@users.noreply.github.com>
2021-04-26 20:14:32 +0200

    Creación de paquetes. Librerías añadidas

--- src/main/java/com/fatdown/spring/FatDownApplication.java
+++ src/main/java/com/fatdown/spring/FatDownApplication.java
@@ -1,4 +1,4 @@
-package com.example.fatdown;
+package com.fatdown.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/fatdown/spring/controladores/IndexController.java
+++ src/main/java/com/fatdown/spring/controladores/IndexController.java
@@ -1,4 +1,4 @@
-package controladores;
+package com.fatdown.spring.controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
