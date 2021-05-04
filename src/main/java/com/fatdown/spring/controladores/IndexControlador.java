63ad775716fddcdced6d859a9860e8fa6e85a9c2
amseguraglez <58846324+amseguraglez@users.noreply.github.com>
2021-05-04 19:06:46 +0200

    IndexControlador hecho

--- src/main/java/com/fatdown/spring/controladores/IndexControlador.java
+++ src/main/java/com/fatdown/spring/controladores/IndexControlador.java
@@ -1,17 +1,23 @@
 package com.fatdown.spring.controladores;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexControlador {
 	@GetMapping({ "/", "index" })
 	public String index(Model model, HttpSession session) {
-		//TODO 	
-		return "";
+		return "index";
+	}
+	
+	@PostMapping("/volver")
+	public String volverAlInicio(HttpServletRequest request) {
+		return "redirect:/index";
 	}
 }
