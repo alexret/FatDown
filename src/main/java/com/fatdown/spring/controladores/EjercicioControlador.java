94fe25866ae2edffa7905d402346e5b6a78ee3bf
alexret <alex.relano18@gmail.com>
2021-05-08 15:27:16 +0200

    Arreglos para crearEjercicio

--- src/main/java/com/fatdown/spring/controladores/EjercicioControlador.java
+++ src/main/java/com/fatdown/spring/controladores/EjercicioControlador.java
@@ -4,10 +4,13 @@ import com.fatdown.spring.entidades.Ejercicio;
 import com.fatdown.spring.servicios.EjercicioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Controller
@@ -17,9 +20,14 @@ public class EjercicioControlador {
     @Autowired
     EjercicioServicio ejercicioServicio;
 
+    @GetMapping("/crearEjercicio")
+    public String creaEjericio(Model model, HttpSession session) {
+        return "ejercicio/crearEjercicio";
+    }
+
     @PostMapping("/crearEjercicio")
-    public String crearEjercicio(@Valid @RequestBody Ejercicio ejercicio) {
+    public String crearEjercicio(@Valid Ejercicio ejercicio) {
         ejercicioServicio.crearEjercicio(ejercicio);
-        return "redirect:/crearEjercicio";
+        return "redirect:/ejercicio/crearEjercicio";
     }
 }
