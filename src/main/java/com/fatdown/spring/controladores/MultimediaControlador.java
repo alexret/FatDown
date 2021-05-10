ceac516b7ca169058c174299f19ab98c61ec3f1d
danie <danie@DESKTOP-6U50I7I>
2021-05-10 11:27:27 +0200

    Métodos controlador Multimedia

--- src/main/java/com/fatdown/spring/controladores/MultimediaControlador.java
+++ src/main/java/com/fatdown/spring/controladores/MultimediaControlador.java
@@ -1,13 +1,17 @@
 package com.fatdown.spring.controladores;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fatdown.spring.entidades.Ejercicio;
 import com.fatdown.spring.entidades.Multimedia;
 import com.fatdown.spring.servicios.MultimediaServicio;
 
@@ -19,10 +23,26 @@ public class MultimediaControlador {
 	@Autowired
 	MultimediaServicio multimediaServicio;
 
+	@GetMapping("/crearMultimedia")
+	public String creaEjericio(Model model, HttpSession session) {
+		return "multimedia/crearMultimedia";
+	}
+
 	@PostMapping("/crearMultimedia")
 	public String crearMultimedia(@Valid @RequestBody Multimedia multimedia) {
 		multimediaServicio.crearMultimedia(multimedia);
 		return "redirect:/crearMultimedia";
 	}
 
+	@PostMapping("/eliminarEjercicio")
+	public String eliminarEjercicio(@Valid Multimedia multimedia) {
+		multimediaServicio.eliminarMultimedia(multimedia.getIdMultimedia());
+		return "redirect:/index";
+	}
+
+	@PostMapping("/listarEjercicios")
+	public String listarEjercicios() {
+		multimediaServicio.listarMultimedia();
+		return "redirect:/index";
+	}
 }
