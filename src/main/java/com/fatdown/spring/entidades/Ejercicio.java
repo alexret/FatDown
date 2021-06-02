063152e088f96cb9ea8568d8535b8cc091df2377
danie <danie@DESKTOP-6U50I7I>
2021-06-02 17:49:25 +0200

    Arreglado Ejercicios

--- src/main/java/com/fatdown/spring/entidades/Ejercicio.java
+++ src/main/java/com/fatdown/spring/entidades/Ejercicio.java
@@ -22,54 +22,57 @@ import com.fatdown.spring.enums.Categoria;
 @Table(name = "EJERCICIO")
 public class Ejercicio implements Serializable {
 
-    private static final long serialVersionUID = -8548755844378572454L;
+	private static final long serialVersionUID = -8548755844378572454L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID_EJERCICIO")
-    private Long idEjercicio;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_EJERCICIO")
+	private Long idEjercicio;
 
-    // Datos
-    @Column(name = "NOMBRE_EJERCICIO")
-    @NotNull(message = "El nombre del ejercicio no puede ser nulo")
-    @NotBlank(message = "El nombre del ejercicio no puede estar vacío")
-    private String nombreEjercicio;
+	// Datos
+	@Column(name = "NOMBRE_EJERCICIO")
+	@NotNull(message = "El nombre del ejercicio no puede ser nulo")
+	@NotBlank(message = "El nombre del ejercicio no puede estar vacío")
+	private String nombreEjercicio;
 
-    @Column(name = "DESCRIPCION_EJERCICIO")
-    @NotNull(message = "La descripción del ejercicio no puede ser nulo")
-    @NotBlank(message = "La descripción no puede estar vacía")
-    private String descripcionEjercicio;
+	@Column(name = "DESCRIPCION_EJERCICIO")
+	@NotNull(message = "La descripción del ejercicio no puede ser nulo")
+	@NotBlank(message = "La descripción no puede estar vacía")
+	private String descripcionEjercicio;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "CATEGORIA_EJERCICIO")
-    @NotNull(message = "La categoría del ejercicio no puede ser nula")
-    @NotBlank(message = "La categoría no puede estar vacía")
-    private Categoria categoriaEjercicio;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "CATEGORIA_EJERCICIO")
+	@NotNull(message = "La categoría del ejercicio no puede ser nula")
+	@NotBlank(message = "La categoría no puede estar vacía")
+	private Categoria categoriaEjercicio;
 
-    @Column(name = "TIPO_EJERCICIO")
-    @NotNull(message = "El tipo de ejercicio no puede ser nulo")
-    @NotBlank(message = "El tipo de ejercicio no puede estar vacío")
-    private String tipoEjercicio;
+	@Column(name = "TIPO_EJERCICIO")
+	@NotNull(message = "El tipo de ejercicio no puede ser nulo")
+	@NotBlank(message = "El tipo de ejercicio no puede estar vacío")
+	private String tipoEjercicio;
 
-    @Column(name = "REPETICION_EJERCICIO")
-    private int repeticionEjercicio;
+	@Column(name = "REPETICION_EJERCICIO")
+	private int repeticionEjercicio;
 
-    @Column(name = "DURACION_EJERCICIO")
-    private int duracionEjercicio;
+	@Column(name = "DURACION_EJERCICIO")
+	private int duracionEjercicio;
 
-    // Relación OneToMany
-    @OneToMany(mappedBy="ejercicio",
-            cascade = { CascadeType.PERSIST, CascadeType.MERGE },
-            orphanRemoval=true)
-    private Set<Rutina> rutina = new HashSet<>();
+	// Relación OneToMany
+	@OneToMany(mappedBy = "ejercicio", cascade = { CascadeType.PERSIST, CascadeType.MERGE }, orphanRemoval = true)
+	private Set<Rutina> rutina = new HashSet<>();
 
 	@OneToOne(mappedBy = "ejercicio", cascade = CascadeType.ALL)
 	private Gif gif;
+	
+	
+
+	public Ejercicio() {
+		super();
+	}
 
 	// Constructor con todos los campos pero sin ID
 	public Ejercicio(String nombreEjercicio, String descripcionEjercicio, Categoria categoriaEjercicio,
-			 String tipoEjercicio, int repeticionEjercicio, int duracionEjercicio,
-			Set<Rutina> rutina) {
+			String tipoEjercicio, int repeticionEjercicio, int duracionEjercicio, Set<Rutina> rutina) {
 		super();
 		this.nombreEjercicio = nombreEjercicio;
 		this.descripcionEjercicio = descripcionEjercicio;
@@ -79,14 +82,21 @@ public class Ejercicio implements Serializable {
 		this.duracionEjercicio = duracionEjercicio;
 		this.rutina = rutina;
 	}
-	
+
+	public Ejercicio(String nombreEjercicio, String descripcionEjercicio, Categoria categoriaEjercicio,
+			String tipoEjercicio, int repeticionEjercicio, int duracionEjercicio) {
+		super();
+		this.nombreEjercicio = nombreEjercicio;
+		this.descripcionEjercicio = descripcionEjercicio;
+		this.categoriaEjercicio = categoriaEjercicio;
+		this.tipoEjercicio = tipoEjercicio;
+		this.repeticionEjercicio = repeticionEjercicio;
+		this.duracionEjercicio = duracionEjercicio;
+	}
+
 	// Constructor con todos los campos menos duracionEjercicio
-	public Ejercicio(
-			String nombreEjercicio,
-			String descripcionEjercicio,
-			Categoria categoriaEjercicio,
-			String tipoEjercicio,
-			int repeticionEjercicio, Set<Rutina> rutina, Gif gif) {
+	public Ejercicio(String nombreEjercicio, String descripcionEjercicio, Categoria categoriaEjercicio,
+			String tipoEjercicio, int repeticionEjercicio, Set<Rutina> rutina, Gif gif) {
 		super();
 		this.nombreEjercicio = nombreEjercicio;
 		this.descripcionEjercicio = descripcionEjercicio;
@@ -96,14 +106,21 @@ public class Ejercicio implements Serializable {
 		this.rutina = rutina;
 		this.gif = gif;
 	}
-	
+
+	public Ejercicio(String nombreEjercicio, String descripcionEjercicio, Categoria categoriaEjercicio,
+			String tipoEjercicio, int repeticionEjercicio, Gif gif) {
+		super();
+		this.nombreEjercicio = nombreEjercicio;
+		this.descripcionEjercicio = descripcionEjercicio;
+		this.categoriaEjercicio = categoriaEjercicio;
+		this.tipoEjercicio = tipoEjercicio;
+		this.repeticionEjercicio = repeticionEjercicio;
+		this.gif = gif;
+	}
+
 	// Constructor con todos los campos menos repeticionEjercicio
-	public Ejercicio(Long idEjercicio,
-			String nombreEjercicio,
-			String descripcionEjercicio,
-			Categoria categoriaEjercicio,
-			String tipoEjercicio,
-			int duracionEjercicio, Set<Rutina> rutina, Gif gif) {
+	public Ejercicio(Long idEjercicio, String nombreEjercicio, String descripcionEjercicio,
+			Categoria categoriaEjercicio, String tipoEjercicio, int duracionEjercicio, Set<Rutina> rutina, Gif gif) {
 		super();
 		this.idEjercicio = idEjercicio;
 		this.nombreEjercicio = nombreEjercicio;
@@ -183,7 +200,14 @@ public class Ejercicio implements Serializable {
 		return serialVersionUID;
 	}
 
-    
+	public Gif getGif() {
+		return gif;
+	}
 
+	public void setGif(Gif gif) {
+		this.gif = gif;
+	}
+	
+	
 
 }
