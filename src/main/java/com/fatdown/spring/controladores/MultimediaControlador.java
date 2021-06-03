a65d13939105d56035e7e544b02f72e6d7ae5c0c
danie <danie@DESKTOP-6U50I7I>
2021-06-03 20:45:07 +0200

    PaginaciónVideos a medias

--- src/main/java/com/fatdown/spring/controladores/MultimediaControlador.java
+++ src/main/java/com/fatdown/spring/controladores/MultimediaControlador.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -67,21 +68,38 @@ public class MultimediaControlador {
 	}
 
 	@PostMapping("/eliminarVideo/{id}")
-    public String eliminarVideo(@PathVariable("id") long id) {
-        videoServicio.eliminarVideo(id);
-        return "redirect:/index";
-    }
+	public String eliminarVideo(@PathVariable("id") long id) {
+		videoServicio.eliminarVideo(id);
+		return "redirect:/index";
+	}
 
 	@GetMapping("/listarVideos")
-    public ModelAndView listarVideos() {
-        ModelAndView mav = new ModelAndView();
-        Pageable paging = PageRequest.of(0,10);
-        Page<Video> lVideos = videoServicio.listarVideosPaginados(paging);
+	public ModelAndView listarVideos() {
 
-        mav.addObject("video", lVideos);
-        mav.setViewName("/listaVideos");
-        return mav;
-    }
+		ModelAndView mav = new ModelAndView();
+
+		// Se crean 10 pageables con 10 items cada una ordenadas por la categoría
+		Pageable pagina1 = PageRequest.of(0, 40, Sort.by("categoriaVideo").ascending());
+		Pageable pagina2 = PageRequest.of(1, 10, Sort.by("categoriaVideo").ascending());
+		Pageable pagina3 = PageRequest.of(2, 10, Sort.by("categoriaVideo").ascending());
+		Pageable pagina4 = PageRequest.of(3, 10, Sort.by("categoriaVideo").ascending());
+
+		// Se usa el servicio para devolver las páginas con los objetos pageables
+		// creados
+		Page<Video> primera = videoServicio.listarVideosPaginados(pagina1);
+		Page<Video> segunda = videoServicio.listarVideosPaginados(pagina2);
+		Page<Video> tercera = videoServicio.listarVideosPaginados(pagina3);
+		Page<Video> cuarta = videoServicio.listarVideosPaginados(pagina4);
+
+		// Se agregan al modelo las páginas
+		mav.addObject("video", primera);
+		mav.addObject("segunda", segunda);
+		mav.addObject("tercera", tercera);
+		mav.addObject("cuarta", cuarta);
+
+		mav.setViewName("/listaVideos");
+		return mav;
+	}
 
 	// Métodos de Imagen
 
